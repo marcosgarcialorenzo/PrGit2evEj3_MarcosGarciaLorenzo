19624d77919661af7b0d6593119f16a0d51d4b94
Marcos Garcia Lorenzo <marcos.garcialorenzo@alumno.iesluisvives.org>
2025-03-19 14:22:05 +0100

    ver3

--- src/Empleado.java
+++ src/Empleado.java
@@ -2,4 +2,7 @@ public class Empleado {
     private String nombre;
     private int edad;
     private double sueldo;
+    public void incrementarSueldo(int porcentajeIncremento) {
+        this.sueldo += sueldo*(1+porcentajeIncremento/100.0);
+    }
 }
