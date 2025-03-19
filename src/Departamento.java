80dd869bf7bed5593da03ca826fe6cc80cba29e6
Marcos Garcia Lorenzo <marcos.garcialorenzo@alumno.iesluisvives.org>
2025-03-19 14:06:52 +0100

    ver1

--- src/Departamento.java
+++ src/Departamento.java
@@ -1,10 +1,3 @@
-import lombok.Data;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Departamento {
     private String nombre;
     private int numEmpleados;
--- src/Empleado.java
+++ src/Empleado.java
@@ -1,10 +1,3 @@
-import lombok.Data;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Empleado {
     private String nombre;
     private int edad;
